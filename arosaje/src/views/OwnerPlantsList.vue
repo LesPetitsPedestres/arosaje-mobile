4e1c36cda43ffbf946929d32de455b73e5fc14df
Keryanne <kerysie@gmail.com>
2023-02-11 11:47:36 +0100

    Create OwnerPlantsList.vue

---
+++ arosaje/src/views/OwnerPlantsList.vue
@@ -0,0 +1,26 @@
+<template>
+    <ion-page>
+
+    </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonPage } from '@ionic/vue';
+
+export default defineComponent({
+  components: {
+    IonPage
+  },
+
+  setup() {
+    return {
+    }
+  }
+ 
+})
+</script>
+
+<style>
+
+</style>
