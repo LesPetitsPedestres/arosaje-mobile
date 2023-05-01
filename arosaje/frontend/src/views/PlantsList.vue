85a3c554bd6c3254a5136238aa65a9d1aabd7590
Keryanne <47926567+Keryanne@users.noreply.github.com>
2023-05-01 22:01:28 +0200

    Modif liste des plante avec ajout de la Map

--- arosaje/frontend/src/views/PlantsList.vue
+++ arosaje/frontend/src/views/PlantsList.vue
@@ -12,18 +12,36 @@
             <ion-toolbar>
               <ion-searchbar color="secondary" show-clear-button="always" :animated="true" placeholder="Rechercher"></ion-searchbar>
             </ion-toolbar>
+            <ion-toolbar>
+              <ion-segment v-model="currentContent" @ionChange="changeContent">
+                <ion-segment-button value="list">
+                  <ion-icon :icon="listOutline"></ion-icon>
+                </ion-segment-button>
+                <ion-segment-button value="map">
+                  <ion-icon :icon="mapOutline"></ion-icon>
+                </ion-segment-button>
+              </ion-segment>
+            </ion-toolbar>
           </div>
         </ion-header>
-        <div v-for="plante in plantes" :key="plante.ID">
-          <PlantCard :plant_name="plante.name" :plant_adress="plante.address" :plant_photo="plante.photo_path" :plant_id="plante.ID"/>
+        <div v-if="currentContent === 'list'">
+          <div v-for="plante in plantes" :key="plante.ID">
+            <PlantCard :plant_name="plante.name" :plant_adress="plante.address" :plant_photo="plante.photo_path" :plant_id="plante.ID"/>
+          </div>
         </div>
+        <div v-else-if="currentContent === 'map'">
+          <Map :apiKey="apiKey" :addressList="addressList" />
+        </div>
+
       </ion-page>
   </template>
   
   <script lang="ts">
   import { defineComponent } from 'vue'
-  import { IonPage, IonHeader, IonToolbar, IonTitle, IonSearchbar, IonButtons, IonMenuButton } from '@ionic/vue'
-  import PlantCard from '../components/PlantCard.vue'
+  import { IonPage, IonHeader, IonToolbar, IonTitle, IonSearchbar, IonButtons, IonMenuButton, IonSegment, IonSegmentButton, IonIcon } from '@ionic/vue'
+  import { mapOutline,listOutline } from 'ionicons/icons';
+  import PlantCard from '../components/PlantCard.vue';
+  import Map from '../components/Map.vue';
   
   import axios from 'axios'
   
@@ -37,14 +55,43 @@
   
   export default defineComponent({
     components: {
-      IonPage, IonHeader, IonToolbar, IonTitle, IonSearchbar, IonButtons, IonMenuButton,
-      PlantCard,
+      IonPage, IonHeader, IonToolbar, IonTitle, IonSearchbar, IonButtons, IonMenuButton, IonSegment, IonSegmentButton, IonIcon,
+      PlantCard, Map
+    },
+
+    setup() {
+      return {
+        mapOutline, listOutline 
+      }
     },
   
     data() {
       return {
+        apiKey: 'AIzaSyBfqAMKCMdaV9KQnsM-8uKlguTH-36cyDg',
+        addressList: [],
         plantes:[] as PlanteResponse[],
+        currentContent: 'list',
       };
+    },
+
+    async created() {
+      await this.fetchAddresses();
+    },
+
+    methods: {
+      changeContent() {
+        console.log('Segment changed:', this.currentContent)
+      },
+
+      async fetchAddresses() {
+        try {
+          const response = await fetch('http://localhost:3000/plants');
+          const data = await response.json();
+          this.addressList = data.map((plant: any) => plant.address);
+        } catch (error) {
+          console.error(error);
+        }
+      },
     },
   
     mounted() {
@@ -54,9 +101,8 @@
         })
         .catch(error => {
           console.error(error);
-        })
-    },
-  
+      })
+    },  
   })
   </script>
   
